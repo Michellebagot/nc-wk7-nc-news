fe9f48b1b3d6d04135c4cdb9266bb107088628e0
Michellebagot <121902516+Michellebagot@users.noreply.github.com>
2024-02-23 13:59:20 +0000

    Task 15 complete - added sort by and order by functionality to articles get point

--- models/articles.model.js
+++ models/articles.model.js
@@ -3,13 +3,36 @@ const db = require("../db/connection");
 exports.selectArticleById = (article) => {
   const articleArray = [article.article_id];
   return db
-    .query(`  SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.body, article_img_url, CAST(COUNT (comments.article_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;`, articleArray)
+    .query(
+      `  SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.body, article_img_url, CAST(COUNT (comments.article_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;`,
+      articleArray
+    )
     .then((result) => {
       return result.rows;
     });
 };
 
-exports.selectAllArticles = (topicQuery) => {
+exports.selectAllArticles = (
+  topicQuery,
+  sortBy = "created_at",
+  orderBy = "desc"
+) => {
+  if (
+    (![
+      "author",
+      "title",
+      "article_id",
+      "topics",
+      "created_at",
+      "votes",
+      "comment_count",
+    ].includes(sortBy) &&
+      sortBy !== undefined) ||
+    (!["asc", "desc"].includes(orderBy) && orderBy !== undefined)
+  ) {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+
   let queryValues = [];
   let queryString =
     "SELECT articles.author, title, articles.article_id, articles.topic, articles.created_at, articles.votes, article_img_url, CAST(COUNT (comments.article_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id";
@@ -19,8 +42,7 @@ exports.selectAllArticles = (topicQuery) => {
     queryString += " WHERE topic = $1";
   }
 
-  queryString +=
-    " GROUP BY articles.article_id ORDER BY articles.created_at DESC;";
+  queryString += ` GROUP BY articles.article_id ORDER BY articles.${sortBy} ${orderBy};`;
 
   return db.query(queryString, queryValues).then((result) => {
     return result.rows;
